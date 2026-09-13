80a956e750568762833fd18a7a1bdd8500b20b83
startup01012 <startup01012@gmail.com>
2026-09-13 12:39:49 +0530

    export forecast backtesting utilities

--- services/forecasting/__init__.py
+++ services/forecasting/__init__.py
@@ -1,4 +1,14 @@
+from .backtest import BacktestCase, BacktestMetrics, evaluate_cases, run_backtest
 from .base import ForecastAdapter, ForecastRequest, ForecastResult
 from .chronos import Chronos2Adapter
 
-__all__ = ["ForecastAdapter", "ForecastRequest", "ForecastResult", "Chronos2Adapter"]
+__all__ = [
+    "ForecastAdapter",
+    "ForecastRequest",
+    "ForecastResult",
+    "Chronos2Adapter",
+    "BacktestCase",
+    "BacktestMetrics",
+    "evaluate_cases",
+    "run_backtest",
+]
