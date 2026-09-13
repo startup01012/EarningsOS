b026d305219bb3cca0e4f167a2a3a05e8087a009
startup01012 <startup01012@gmail.com>
2026-09-13 12:17:29 +0530

    add pretrained forecasting adapter foundation

---
+++ services/forecasting/__init__.py
@@ -0,0 +1,4 @@
+from .base import ForecastAdapter, ForecastRequest, ForecastResult
+from .chronos import Chronos2Adapter
+
+__all__ = ["ForecastAdapter", "ForecastRequest", "ForecastResult", "Chronos2Adapter"]
