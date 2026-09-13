8398decb1b9ff8f8d41865a506f92e6840808b6d
startup01012 <startup01012@gmail.com>
2026-09-13 13:44:32 +0530

    Export Kronos-small forecasting adapter

--- services/forecasting/__init__.py
+++ services/forecasting/__init__.py
@@ -1,12 +1,14 @@
 from .backtest import BacktestCase, BacktestMetrics, evaluate_cases, run_backtest
 from .base import ForecastAdapter, ForecastRequest, ForecastResult
 from .chronos import Chronos2Adapter
+from .kronos import KronosSmallAdapter
 
 __all__ = [
     "ForecastAdapter",
     "ForecastRequest",
     "ForecastResult",
     "Chronos2Adapter",
+    "KronosSmallAdapter",
     "BacktestCase",
     "BacktestMetrics",
     "evaluate_cases",
