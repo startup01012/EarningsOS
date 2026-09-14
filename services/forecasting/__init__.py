6557846d797abadb4dbb2ae6d677ca72cb1d2496
startup01012 <startup01012@gmail.com>
2026-09-14 19:41:36 +0530

    Export FinCast and TimesFM adapters

--- services/forecasting/__init__.py
+++ services/forecasting/__init__.py
@@ -1,14 +1,18 @@
 from .backtest import BacktestCase, BacktestMetrics, evaluate_cases, run_backtest
 from .base import ForecastAdapter, ForecastRequest, ForecastResult
 from .chronos import Chronos2Adapter
+from .fincast import FinCastAdapter
 from .kronos import KronosSmallAdapter
+from .timesfm import TimesFM25Adapter
 
 __all__ = [
     "ForecastAdapter",
     "ForecastRequest",
     "ForecastResult",
     "Chronos2Adapter",
+    "FinCastAdapter",
     "KronosSmallAdapter",
+    "TimesFM25Adapter",
     "BacktestCase",
     "BacktestMetrics",
     "evaluate_cases",
